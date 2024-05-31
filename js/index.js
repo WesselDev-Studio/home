930c3c420e523ffc8c4570adc36064b3ab7c5c41
WesselDev202 <123660299+WesselDev202@users.noreply.github.com>
2024-05-31 16:08:57 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
 function CB() {
-    window.location='../html/cuber.html';
+    window.location='./html/cuber.html';
 };
 
 function C2B() {
-    window.location='../html/cuber-2.html';
+    window.location='./html/cuber-2.html';
 };
 
 function C2RB() {
-    window.location='../html/cuber-2r.html';
+    window.location='./html/cuber-2r.html';
 };
 
 function C3B() {
-    window.location='../html/cuber-3.html';
+    window.location='./html/cuber-3.html';
 };
 
 function DSCB() {
-    window.location='../html/survivalcraft.html';
+    window.location='./html/survivalcraft.html';
 };
 
 function HOWB() {
-    window.location='../html/under-con.html'
+    window.location='./html/under-con.html'
 }

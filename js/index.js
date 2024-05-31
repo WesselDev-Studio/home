d172d35a91fac0748c7577e910df867bc94f72a4
WesselDev202 <123660299+WesselDev202@users.noreply.github.com>
2024-05-31 16:00:23 +0200

    Updated Website

--- js/index.js
+++ js/index.js
@@ -1,19 +1,23 @@
 function CB() {
-    window.location='./html/under-con.html';
+    window.location='../html/cuber.html';
 };
 
 function C2B() {
-    window.location='./html/under-con.html';
+    window.location='../html/cuber-2.html';
 };
 
 function C2RB() {
-    window.location='./html/under-con.html';
+    window.location='../html/cuber-2r.html';
 };
 
 function C3B() {
-    window.location='./html/under-con.html';
+    window.location='../html/cuber-3.html';
 };
 
 function DSCB() {
-    window.location='./html/under-con.html';
+    window.location='../html/survivalcraft.html';
 };
+
+function HOWB() {
+    window.location='../html/under-con.html'
+}
--- js/mag.js
+++ js/mag.js
@@ -2,10 +2,22 @@ function HB() {
     window.location='../index.html'
 }
 
+function DB() {
+    window.location='https://discord.gg/pyba5KKgHx';
+};
+
 function CB() {
     window.location='https://wesseldev-inc.itch.io/cuber';
 };
 
 function C2B() {
     window.location='https://wesseldev-inc.itch.io/cuber-2-ice-age';
+};
+
+function C2RB() {
+    window.location='https://wesseldev-inc.itch.io/cuber-2-ice-age-remake';
+};
+
+function DSCB() {
+    window.location='https://wesseldev-inc.itch.io/2d-survivalcraft';
 };

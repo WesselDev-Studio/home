51d34d4e78fcad7784c338df8fea9b9d57a2c4ac
WesselDev202 <123660299+WesselDev202@users.noreply.github.com>
2024-05-31 12:49:01 +0200

    Update page

---
+++ js/index.js
@@ -0,0 +1,19 @@
+function CB() {
+    window.location='/html/under-con.html';
+};
+
+function C2B() {
+    window.location='/html/under-con.html';
+};
+
+function C2RB() {
+    window.location='/html/under-con.html';
+};
+
+function C3B() {
+    window.location='/html/under-con.html';
+};
+
+function DSCB() {
+    window.location='/html/under-con.html';
+};
---
+++ js/mag.js
@@ -0,0 +1,11 @@
+function HB() {
+    window.location='../index.html'
+}
+
+function CB() {
+    window.location='https://wesseldev-inc.itch.io/cuber';
+};
+
+function C2B() {
+    window.location='https://wesseldev-inc.itch.io/cuber-2-ice-age';
+};

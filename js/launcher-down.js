60482936f01b50d7766eadd904c3fac705b74e5b
WesselDev202 <123660299+WesselDev202@users.noreply.github.com>
2024-06-02 16:50:36 +0200

    Added Launcher.exe

---
+++ js/launcher-down.js
@@ -0,0 +1,4 @@
+function HB() {
+    window.location='../index.html';
+};
+
--- js/mag.js
+++ js/mag.js
@@ -1,27 +1,7 @@
 function HB() {
-    window.location='../index.html'
+    window.location='../index.html';
+};
+
+function GLB() {
+    window.location='../html/launcher-down.html'
 }
-
-function DB() {
-    window.location='https://discord.gg/pyba5KKgHx';
-};
-
-function CB() {
-    window.location='https://wesseldev-inc.itch.io/cuber';
-};
-
-function C2B() {
-    window.location='https://wesseldev-inc.itch.io/cuber-2-ice-age';
-};
-
-function C2RB() {
-    window.location='https://wesseldev-inc.itch.io/cuber-2-ice-age-remake';
-};
-
-function DSCB() {
-    window.location='https://wesseldev-inc.itch.io/2d-survivalcraft';
-};
-
-function THB() {
-    window.location='https://wesseldev-inc.itch.io/the-healer';
-};

b1ef71f8279eb8fe1398fcb61da4d58c86754a21
WesselDev202 <wessel240609@gmail.com>
2024-06-13 13:41:56 +0200

    Update Website

--- js/countdown-timer/cuber2-countdown.js
+++ js/countdown-timer/cuber2-countdown.js
@@ -1,7 +1,7 @@
-var countDownDate = new Date("Jun 8, 2024 12:00:00").getTime();
+var countDownDateCuber2 = new Date("Jun 8, 2024 12:00:00").getTime();
 var x = setInterval(function() {
     var now = new Date().getTime();
-    var distance = countDownDate - now;
+    var distance = countDownDateCuber2 - now;
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 *24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
@@ -11,6 +11,6 @@ var x = setInterval(function() {
 
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("cuber2-timer").innerHTML = "EXPIRED";
+        document.getElementById("cuber2-timer").innerHTML = "Cuber 2: Ice Age: " + "EXPIRED";
     }
 }, 1000);
